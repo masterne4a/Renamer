d55a7301464261c32d0fccecb759b935b473816f
Mike from SPb <b18112016@yandex.ru>
2025-11-18 10:01:59 +0300

    feat: Добавил кнопки, и обработчик. Отредактировал readme

--- src/main.js
+++ src/main.js
@@ -1,26 +1,41 @@
-import './style.css'
-import './globals.css'
-import './normalize.css'
-import { rename } from './renamer.js'
+import "./style.css";
+import "./globals.css";
+import "./normalize.css";
+import { rename, events } from "./renamer.js";
 
-document.querySelector('#app').innerHTML = `
-<form class="mainForm">  
-    <label for="address">Введи адрес папки:</label>
-    <input class="address" name="address"></input>
-    <output role="status" class="avitoAddress" name="avitoAddress" id="avitoAddress" for="address"></output>
+document.querySelector("#app").innerHTML = `
+<form id="checboxForm">
+    <label>
+        <span class="label-span">Товары</span>
+        <input type="radio" name="checPlase" class="product-radio" checked>
+    </label>
+    <label>
+        <span class="label-span">Услуги</span>
+        <input type="radio" name="checPlase" class="service-radio">
+    </label>
 </form>
-`
+<form class="mainForm" id="mainForm">  
+    <label for="address" class="input-label">
+        <span class="input-title">Имя папки:</span>
+        <input class="address" name="address" type="text" placeholder="введите адрес папки">
+    </label>
+    <output for="address" form="mainForm" name="avitoAddress" role="status" type="text" class="avitoAddress" id="avitoAddress" placeholder="результат"></output>
+</form>
+`;
+document.addEventListener("DOMContentLoaded", function () {
+  events();
+});
 
-const form = document.querySelector('.mainForm');
-const addressInput = document.querySelector('.address');
-const addressOutput = document.querySelector('.avitoAddress');
+const form = document.querySelector(".mainForm");
+const addressInput = document.querySelector(".address");
+const addressOutput = document.querySelector(".avitoAddress");
 
-addressInput.addEventListener('input', function(){
-const value = this.value
-addressOutput.textContent = rename(value)
-})
+addressInput.addEventListener("input", function () {
+  const value = this.value;
+  addressOutput.textContent = rename(value);
+  if (!value){
+    addressOutput.textContent = "";
+  }
+});
 
-
-
-
-console.log(rename('28.датчик-A6-C7-A8-D4Macan-Rear-DCM/1-1.jpg'))
+console.log(rename("28.датчик-A6-C7-A8-D4Macan-Rear-DCM/1-1.jpg"));
--- src/renamer.js
+++ src/renamer.js
@@ -1,7 +1,7 @@
-export function rename (elem) {
-  
-  const firsPart = 'yandex_disk://ПневмоСПБ-Товары/';
-  let str = firsPart + elem
+let firsPart = "yandex_disk://ПневмоСПБ-Товары/";
+
+export function rename(elem) {
+  let str = firsPart + elem;
   const str1 = str + `/1-1.jpg | `;
   const str2 = str + `/1-2.jpg | `;
   const str3 = str + `/1-3.jpg | `;
@@ -10,5 +10,26 @@ export function rename (elem) {
   const str6 = str + `/1-6.jpg | `;
   const str7 = str + `/1-7.jpg | `;
   const str8 = str + `/1-8.jpg`;
-  return str1.concat(str2, str3, str4, str5, str6, str7, str8)
+  return str1.concat(str2, str3, str4, str5, str6, str7, str8);
+}
+
+export function events() {
+  const productRadio = document.querySelector(".product-radio");
+  const serviceRadio = document.querySelector(".service-radio");
+
+  if (productRadio) {
+    productRadio.addEventListener("change", function () {
+      if (this.checked) {
+        firsPart = "yandex_disk://ПневмоСПБ-Товары/";
+      }
+    });
+  }
+
+  if (serviceRadio) {
+    serviceRadio.addEventListener("change", function () {
+      if (this.checked) {
+        firsPart = "yandex_disk://ПневмоСПБ-Услуги/";
+      }
+    });
+  }
 }
